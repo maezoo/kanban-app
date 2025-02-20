6046b93a46d498ccf2db6bfcb254398b5116ca4a
maezoo <aa00456@naver.com>
2025-02-20 19:53:23 +0900

    7.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { useRecoilState, useRecoilValue } from "recoil";
+import { minsuteState, hoursSelector } from "./atoms";
+
 function App() {
-  return <div></div>;
+  const [minutes, setMinutes] = useRecoilState(minsuteState);
+  const hours = useRecoilValue(hoursSelector);
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+  };
+  return (
+    <div>
+      <input
+        value={minutes}
+        onChange={onMinutesChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input value={hours} type="number" placeholder="Hours" />
+    </div>
+  );
 }
 
 export default App;
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,1 +1,14 @@
 import { atom, selector } from "recoil";
+
+export const minsuteState = atom({
+  key: "minutes",
+  default: 0,
+});
+
+export const hoursSelector = selector({
+  key: "hours",
+  get: ({ get }) => {
+    const minutes = get(minsuteState);
+    return minutes / 60;
+  },
+});
