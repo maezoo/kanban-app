cacf272fe36e3331243183b72c5f8f534b09d60c
maezoo <aa00456@naver.com>
2025-02-21 21:09:25 +0900

    7.5

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,18 +1,6 @@
 import { atom, selector } from "recoil";
 
-export const minsuteState = atom({
-  key: "minutes",
-  default: 0,
-});
-
-export const hoursSelector = selector<number>({
-  key: "hours",
-  get: ({ get }) => {
-    const minutes = get(minsuteState);
-    return minutes / 60;
-  },
-  set: ({ set }, newValue) => {
-    const minutes = Number(newValue) * 60;
-    set(minsuteState, minutes);
-  },
+export const toDoState = atom({
+  key: "toDo",
+  default: ["a", "b", "c", "d", "e", "f"],
 });
